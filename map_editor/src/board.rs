9e4d30165ba7deed4f83c185a34ac7d0630dfbf6
Ross Andrews <ross.andrews@gmail.com>
2024-12-15 02:47:38 -0600

    click to add remove walls

--- map_editor/src/board.rs
+++ map_editor/src/board.rs
@@ -1,3 +1,87 @@
-pub struct Board {
+use bananagraph::Sprite;
+use grid::{xy, Coord, Grid, GridMut};
 
+#[derive(Copy, Clone, Debug)]
+pub enum Cell {
+    White,
+    Black,
+    ShortWall,
+    TallWall,
+    Blank
+}
+
+impl Into<Sprite> for Cell {
+    fn into(self) -> Sprite {
+        match self {
+            Cell::White => Sprite::new((320, 0), (32, 48)),
+            Cell::Black => Sprite::new((352, 0), (32, 48)),
+            Cell::ShortWall => Sprite::new((320, 48), (32, 48)),
+            Cell::TallWall => Sprite::new((0, 96), (32, 48)),
+            Cell::Blank => Sprite::new((0, 0), (32, 48)),
+        }
+    }
+}
+
+pub struct Board {
+    width: i32,
+    height: i32,
+    cells: Vec<Cell>
+}
+
+impl Board {
+    pub fn new(width: i32, height: i32) -> Self {
+        let mut cells = vec![Cell::Blank; (width * height) as usize];
+
+        for (n, cell) in cells.iter_mut().enumerate() {
+            let (x, y) = (n % width as usize, n / width as usize);
+            *cell = if x == 0 || y == 0 {
+                Cell::TallWall
+            } else if x == (width - 1) as usize || y == (height - 1) as usize {
+                Cell::ShortWall
+            } else {
+                Board::square_color((x as i32, y as i32))
+            }
+        }
+
+        Self { width, height, cells }
+    }
+
+    pub fn square_color(coord: (i32, i32)) -> Cell {
+        let (x, y) = coord;
+        if (x + y) % 2 == 0 {
+            Cell::White
+        } else {
+            Cell::Black
+        }
+    }
+}
+
+impl Grid for Board {
+    type CellType = Cell;
+
+    fn size(&self) -> Coord {
+        xy(self.width, self.height)
+    }
+
+    fn default(&self) -> Self::CellType {
+        Cell::Blank
+    }
+
+    fn get(&self, index: Coord) -> Option<&Self::CellType> {
+        if self.contains(index) {
+            Some(&self.cells[(index.0 + index.1 * self.width) as usize])
+        } else {
+            None
+        }
+    }
+}
+
+impl GridMut for Board {
+    fn get_mut(&mut self, index: Coord) -> Option<&mut Self::CellType> {
+        if self.contains(index) {
+            Some(&mut self.cells[(index.0 + index.1 * self.width) as usize])
+        } else {
+            None
+        }
+    }
 }
--- map_editor/src/main.rs
+++ map_editor/src/main.rs
@@ -4,6 +4,8 @@ use winit::dpi::LogicalSize;
 use winit::error::EventLoopError;
 use winit::event::{ElementState, Event, MouseButton, StartCause, WindowEvent};
 use winit::event_loop::ControlFlow;
+use grid::{xy, Grid, GridMut};
+use crate::board::{Board, Cell};
 
 mod board;
 
@@ -26,6 +28,9 @@ pub async fn run_window() -> Result<(), EventLoopError> {
     // space as the window, so just floor()ing it gives you reasonable coordinates
     let mut mouse_pos: (f64, f64) = (-1f64, -1f64);
 
+    // Make a 10x10 board:
+    let mut board = Board::new(10, 10);
+
     event_loop.run(move |event, target| {
         match event {
             // Exit if we click the little x
@@ -34,7 +39,9 @@ pub async fn run_window() -> Result<(), EventLoopError> {
             }
 
             // Redraw if it's redrawing time
-            Event::WindowEvent { event: WindowEvent::RedrawRequested, window_id } if window_id == our_id => { wrapper.redraw(&board_sprites()); },
+            Event::WindowEvent { event: WindowEvent::RedrawRequested, window_id } if window_id == our_id => {
+                redraw_window(&wrapper, &board, mouse_pos);
+            },
 
             // Resize if it's resizing time
             Event::WindowEvent { event: WindowEvent::Resized(_), window_id } if window_id == our_id => wrapper.handle_resize(),
@@ -46,7 +53,7 @@ pub async fn run_window() -> Result<(), EventLoopError> {
 
             // When the timer fires, redraw thw window and restart the timer (update will go here)
             Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
-                wrapper.redraw_with_ids(board_sprites()).expect("Drawing error");
+                redraw_window(&wrapper, &board, mouse_pos);
                 target.set_control_flow(ControlFlow::WaitUntil(Instant::now() + timer_length));
             }
 
@@ -63,7 +70,7 @@ pub async fn run_window() -> Result<(), EventLoopError> {
             } if window_id == our_id => {
                 let ids = wrapper.get_sprite_ids().unwrap();
                 let id = ids[mouse_pos.into()];
-                println!("Sprite id: {}", id);
+                toggle_wall(id, &mut board)
             }
 
             _ => {} // toss the others
@@ -71,44 +78,115 @@ pub async fn run_window() -> Result<(), EventLoopError> {
     })
 }
 
-fn board_sprites() -> Vec<Sprite> {
-    let white = Sprite::new((320, 0), (32, 48));
-    let black = Sprite::new((352, 0), (32, 48));
+fn toggle_wall(id: u32, board: &mut Board) {
+    if id >= 100000 {
+        let coord = sprite_id_to_coord(id, board.size().0);
+        let cell = board.get(coord.into()).unwrap();
+        let (x, y) = coord;
 
-    let mut sprites = Vec::new();
+        *board.get_mut(coord.into()).unwrap() = match cell {
+            Cell::Black | Cell::White => {
+                if x == 0 || y == 0 {
+                    Cell::TallWall
+                } else {
+                    Cell::ShortWall
+                }
+            },
+            Cell::ShortWall | Cell::TallWall => {
+                Board::square_color(coord)
+            },
+            _ => unreachable!()
+        }
+    }
+}
 
-    for y in 0..8 {
-        for x in 0..8 {
-            let tile = if y % 2 == 0 { white } else { black }
-                .with_id(x + y * 8 + 100000)
-                .with_z((8.0 - y as f32) / 10.0)
-                .with_position((32.0 * x as f32 + 16.0 * (y % 2) as f32, 8.0 * y as f32), (320.0, 240.0));
+fn redraw_window(wrapper: &GpuWrapper, board: &Board, mouse_pos: (f64, f64)) {
+    let mut sprites = board_sprites(board);
+    let buffer = wrapper.redraw_ids(&sprites).expect("Drawing error");
 
-            sprites.push(tile)
+    if buffer.contains((mouse_pos.0 as u32, mouse_pos.1 as u32).into()) {
+        let id = buffer[mouse_pos.into()];
+        if id >= 100000 {
+            let board_coord = sprite_id_to_coord(id, board.size().0);
+            match board.get(board_coord.into()) {
+                Some(Cell::White | Cell::Black) => {
+                    let highlight = highlight_sprites(board_coord);
+                    sprites.push(highlight.0);
+                    sprites.push(highlight.1);
+                },
+                _ => {}
+            }
         }
     }
 
-    let toph = Sprite::new((416, 0), (32, 48));
-    let btmh = Sprite::new((384, 0), (32, 48));
+    wrapper.redraw(&sprites);
+}
+
+fn sprite_id_to_coord(id: u32, width: i32) -> (i32, i32) {
+    ((id - 100000) as i32 % width, (id - 100000) as i32 / width)
+}
+
+fn coord_to_iso(coord: (i32, i32)) -> (f32, f32) {
+    let (x, y) = coord;
+    let width = 320; // (logical) screen width
+    let (dw, dh) = (16, 8); // how much to shift for one increment of "down" vs "lateral". Depends on the exact tile art
+    let (basex, basey) = ((width / 2 - dw), 0); // Coord of (0, 0) tile at top center
+
+    // for each x, we go right dw and down dh
+    // for each y, we go _left_ dw and down dh
+    ((basex + (x - y) * dw) as f32, (basey + (x + y) * dh) as f32)
+}
+
+/// Return a z coordinate between 0.0 and 1.0 that will stack these tiles prettily.
+/// These will, on a 10x10 grid, range from 0.052 to 0.99, meaning if you add or subtract a
+/// ten-thousandth from them then you'll still be in the same "rank" but tweaking the z-order
+/// within a given cell.
+/// Because this isa ll orthographic projection, we don't really give a damn what the z coordinate
+/// _is,_ as long as they sort correctly; we're only using it for occultation, not perspective.
+fn coord_to_z(coord: (i32, i32)) -> f32 {
+    let (x, y) = coord;
+
+    // The rank of something is its x + y, because isometric view; the lower right of the board
+    // is closest to the "camera". Higher z means it's farther away, so we can just divide something
+    // by x + y. But! We can't have x + y == 0 obviously, so tweak it a bit, and we also can't have
+    // a z of 1.0, because the 0.0..1.0 range is exclusive. So make it a little bit less, 0.99:
+    0.99 / ((x + y + 1) as f32)
+}
+
+fn board_sprites(board: &Board) -> Vec<Sprite> {
+    let mut sprites = Vec::new();
+
+    for (n, cell) in board.iter().enumerate() {
+        let (x, y) = board.coord(n).into();
+        let tile: Sprite = (*cell).into();
+        let screen_pos = coord_to_iso((x, y));
+        let tile = tile.with_id((x + y * board.size().0 + 100000) as u32)
+            .with_z(coord_to_z((x, y)))
+            .with_position(screen_pos, (320.0, 240.0));
+        sprites.push(tile);
+    }
 
     let crate_tile = Sprite::new((256, 288), (32, 48));
-
     let (x, y) = (3, 4);
-    let z = (8.0 - y as f32) / 10.0;
-
-    sprites.push(toph
-        .with_z(z + 0.05)
-        .with_position((32.0 * x as f32 + 16.0 * (y % 2) as f32, 8.0 * y as f32), (320.0, 240.0)));
-
-    sprites.push(btmh
-        .with_z(z - 0.05)
-        .with_position((32.0 * x as f32 + 16.0 * (y % 2) as f32, 8.0 * y as f32), (320.0, 240.0)));
-
+    let screen_pos = coord_to_iso((x, y));
+    let z = coord_to_z((x, y)) - 0.0002; // Move it two epsilon toward the camera, so it's atop the tile itself as well as the (possible) back highlighting
 
     sprites.push(crate_tile
-        .with_z(z - 0.025)
-        .with_position((32.0 * x as f32 + 16.0 * (y % 2) as f32, 8.0 * y as f32), (320.0, 240.0)));
+        .with_z(z)
+        .with_position(screen_pos, (320.0, 240.0)));
+
     sprites
+}
+
+fn highlight_sprites(coord: (i32, i32)) -> (Sprite, Sprite) {
+    let toph = Sprite::new((416, 0), (32, 48));
+    let btmh = Sprite::new((384, 0), (32, 48));
+    let screen_pos = coord_to_iso(coord);
+    let z = coord_to_z(coord);
+    (
+        toph.with_position(screen_pos, (320.0, 240.0)).with_z(z - 0.0001),
+        btmh.with_position(screen_pos, (320.0, 240.0)).with_z(z - 0.0003)
+    )
 }
 
 pub fn main() {
