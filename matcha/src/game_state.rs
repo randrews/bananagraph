2acb9af69ebf1d24c1827a52675b44702b3bcfd7
Ross Andrews <ross.andrews@gmail.com>
2025-01-12 02:05:47 -0600

    hecs

--- matcha/src/game_state.rs
+++ matcha/src/game_state.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 use cgmath::{Deg, Point2};
+use hecs::{Entity, World};
 use rand::Rng;
 use bananagraph::{DrawingContext, Sprite, SpriteId};
 use grid::{Coord, Grid, VecGrid};
@@ -22,26 +23,18 @@ pub enum PieceColor {
 
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Animation {
-    SPIN { angle: Deg<f32>, coord: Coord }
+    SPIN { angle: Deg<f32> }
 }
 
 impl Animation {
-    pub fn applies_to(&self, coord: Coord) -> bool {
+    pub fn tick(&mut self, dt: Duration) {
         match self {
-            Animation::SPIN { coord: c, .. } => *c == coord
-        }
-    }
-
-    pub fn tick(&mut self, board: &mut VecGrid<Piece>, dt: Duration) {
-        match self {
-            Animation::SPIN { angle, coord } => {
+            Animation::SPIN { angle} => {
                 let mut new_angle = *angle + Deg(360.0 * dt.as_millis() as f32 / 1000.0);
-                //println!("anim: {}, {}", coord, angle);
                 if new_angle >= Deg(360.0) {
                     new_angle = Deg(360.0)
                 }
                 *angle = new_angle;
-                board[*coord].angle = new_angle;
             }
         }
     }
@@ -92,34 +85,39 @@ impl Piece {
 }
 
 pub struct GameState<'a, R: Rng> {
-    board: VecGrid<Piece>,
+    world: World,
+    board: VecGrid<Entity>,
     rng: &'a mut R,
-    screen: (u32, u32),
-    animations: Vec<Animation>
+    screen: (u32, u32)
 }
 
 impl<'a, R: Rng> GameState<'a, R> {
     pub fn new(rng: &'a mut R, screen: (u32, u32)) -> Self {
-        let mut board = VecGrid::new((8, 8).into(), Piece::new(PieceColor::RED));
+        let mut world = World::new();
+        let mut board = VecGrid::new((8, 8).into(), Entity::DANGLING);
 
         for coord in board.size() {
-            board[coord] = Piece::new_from_rand(rng);
+            board[coord] = world.spawn((Piece::new_from_rand(rng),));
         }
 
         Self {
+            world,
             board,
             rng,
-            screen,
-            animations: vec![]
+            screen
         }
     }
 
     pub fn tick(&mut self, dt: Duration) {
-        for anim in self.animations.iter_mut() {
-            anim.tick(&mut self.board, dt);
+        let mut finished = vec![];
+        for (ent, (anim,)) in self.world.query_mut::<(&mut Animation,)>() {
+            anim.tick(dt);
+            if anim.finished() { finished.push(ent) }
         }
 
-        self.animations = self.animations.iter().filter_map(|a| if a.finished() { None } else { Some(*a) }).collect();
+        for ent in finished {
+            self.world.remove_one::<Animation>(ent).unwrap();
+        }
     }
 
     pub fn redraw(&self) -> Vec<Sprite> {
@@ -130,12 +128,28 @@ impl<'a, R: Rng> GameState<'a, R> {
             (self.screen.1 as f32 - 8.0 * 85.0) / 2.0
             );
         for (n, coord) in self.board.size().into_iter().enumerate() {
-            let piece = self.board[coord];
-            let sprite = piece.as_sprite().with_z(0.5).with_id(n as u32);
-            let sprite = dc.place_rotated(sprite, (
-                coord.0 as f32 * 85.0 + margin.0,
-                coord.1 as f32 * 85.0 + margin.1
-            ), piece.angle);
+            let mut query = self.world.query_one::<(&Piece,Option<&Animation>)>(self.board[coord]).unwrap();
+            let (piece,anim) = query.get().unwrap();
+
+            let sprite = piece.as_sprite().with_z(0.5);
+            // hecs will give us 0 as a sprite id, but bananagraph can't abide that, so, add something to it to
+            // ensure we can hear clicks on the sprite
+            let sprite = sprite.with_id(self.board[coord].id() + 1000);
+
+            let sprite = match anim {
+                Some(Animation::SPIN { angle }) => {
+                    dc.place_rotated(sprite, (
+                        coord.0 as f32 * 85.0 + margin.0,
+                        coord.1 as f32 * 85.0 + margin.1
+                    ), *angle)
+                }
+                _ => {
+                    dc.place(sprite, (
+                        coord.0 as f32 * 85.0 + margin.0,
+                        coord.1 as f32 * 85.0 + margin.1
+                    ))
+                }
+            };
             sprites.push(sprite)
         }
         sprites
@@ -143,10 +157,29 @@ impl<'a, R: Rng> GameState<'a, R> {
 
     pub fn click(&mut self, target: ClickTarget) {
         if let ClickTarget::SPRITE { id} = target {
-            let coord = self.board.coord(id as usize);
-            if self.animations.iter().find(|a| a.applies_to(coord)).is_none() {
-                self.animations.push(Animation::SPIN { coord, angle: Deg(0.0)})
+            // This is only ever clicked sprite ids, which are always an entity id + 1000: hecs will give 0 as
+            // entity ids, which bananagraph interprets as an empty sprite id
+            let ent = unsafe {
+                self.world.find_entity_from_id(id - 1000)
+            };
+
+            {
+                let mut query = self.world.query_one::<(&Piece,Option<&Animation>)>(ent).unwrap();
+                if let Some((_,Some(anim))) = query.get() {
+                    return
+                }
             }
+
+            // There's no current animation so tack one on:
+            self.world.insert_one(ent, Animation::SPIN { angle: Deg(0.0) }).unwrap();
+
+            // println!("{} - {:?} - {:?}", id, piece.color, anim);
+
+            //let coord = self.board.coord(id as usize);
+            //self.world.
+            // if self.animations.iter().find(|a| a.applies_to(coord)).is_none() {
+            //     self.animations.push(Animation::SPIN { coord, angle: Deg(0.0)})
+            // }
         }
     }
 }
