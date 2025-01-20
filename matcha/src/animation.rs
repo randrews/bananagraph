6f04de1ee051206604f1074c43d065e3e778588b
Ross Andrews <ross.andrews@gmail.com>
2025-01-20 00:43:32 -0600

    wip

--- matcha/src/animation.rs
+++ matcha/src/animation.rs
@@ -1,5 +1,12 @@
 use std::time::Duration;
+use cgmath::Vector2;
+use grid::Coord;
 use crate::drawable::Drawable;
+
+pub trait Animation {
+    fn tick(&mut self, dt: Duration);
+    fn apply_to(&self, drawable: Drawable) -> Drawable;
+}
 
 #[derive(Copy, Clone, Debug)]
 pub struct Pulse {
@@ -14,8 +21,10 @@ impl Pulse {
             delta: 1.0
         }
     }
+}
 
-    pub fn tick(&mut self, dt: Duration) {
+impl Animation for Pulse {
+    fn tick(&mut self, dt: Duration) {
         let bounds = 0.1;
         let mut new_scale = self.scale + self.delta * (bounds * dt.as_millis() as f32 / 200.0);
         if self.delta < 0.0 && new_scale <= 1.0 - bounds {
@@ -28,7 +37,38 @@ impl Pulse {
         self.scale = new_scale;
     }
 
-    pub fn apply_to(&self, drawable: Drawable) -> Drawable {
+    fn apply_to(&self, drawable: Drawable) -> Drawable {
         drawable.with_scale((self.scale, 2.0 - self.scale))
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct MoveAnimation {
+    dest: Vector2<f32>,
+    current: Vector2<f32>,
+    remaining_duration: Duration
+}
+
+impl MoveAnimation {
+    pub fn new(dest: Coord) -> Self {
+        Self {
+            dest: (dest.0 as f32, dest.1 as f32).into(),
+            current: (0.0, 0.0).into(),
+            remaining_duration: Duration::new(1, 0)
+        }
+    }
+
+    pub fn running(&self) -> bool {
+        self.remaining_duration.is_zero()
+    }
+}
+
+impl Animation for MoveAnimation {
+    fn tick(&mut self, dt: Duration) {
+        todo!()
+    }
+
+    fn apply_to(&self, drawable: Drawable) -> Drawable {
+        todo!()
     }
 }
