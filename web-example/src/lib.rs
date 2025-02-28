f60c3ad01626799883dfcfc33b4ca466d6c56f8c
Ross Andrews <ross.andrews@gmail.com>
2025-02-27 22:37:52 -0600

    first working

--- web-example/src/lib.rs
+++ web-example/src/lib.rs
@@ -1,8 +1,7 @@
 use wasm_bindgen::prelude::*;
 use web_sys::HtmlCanvasElement;
 use wgpu::SurfaceTarget;
-use wgpu::VertexStepMode::Instance;
-use bananagraph::GpuWrapper;
+use bananagraph::{GpuWrapper, Sprite};
 
 #[wasm_bindgen(start)]
 pub fn run() {
@@ -12,23 +11,18 @@ pub fn run() {
 
 #[wasm_bindgen]
 pub async fn init_gpu_wrapper() {
-    let wrapper = web_sys::window()
+    let mut wrapper = web_sys::window()
         .and_then(|win| win.document())
         .and_then(|doc| {
             let canvas = doc.get_element_by_id("main_canvas").expect("Expected to find a canvas with id main_canvas");
-
             let canvas: HtmlCanvasElement = canvas.dyn_into().expect("element must be a canvas");
             let size = (canvas.width(), canvas.height()).into();
             let surface_target = SurfaceTarget::Canvas(canvas);
-            // let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-            //     backends: wgpu::Backends::PRIMARY,
-            //     ..Default::default()
-            // });
-            //
-            // let surface = instance.create_surface(surface_target).expect("Failed to create surface");
-
-            // Some(surface)
             Some(GpuWrapper::targeting(surface_target, size))
         })
         .expect("I have no idea what I'm doing").await;
+
+    wrapper.add_texture(include_bytes!("angrytoast.png"), Some("angrytoast.png"));
+    let toast = Sprite::new((0, 0), (320, 240)).with_id(1);
+    let ids = wrapper.redraw_with_ids([toast]).unwrap();
 }
